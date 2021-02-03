e9a4b7651c473588d89c6c1c19d7e76140cf2b51
bche536 <bche536@aucklanduni.ac.nz>
2021-02-04 04:13:43 +1300

    Finished lecture 86

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact={true} path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );

44eec6ac5b563a293fcffdc4619a003d8f3f341d
bche536 <bche536@aucklanduni.ac.nz>
2021-02-07 03:48:18 +1300

    Finished lecture 101

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,34 @@ const config = {
   measurementId: "G-MRFCR3FJPF",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  //If user doesn't exist, create a new user via userAuth obj
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  //Chance we want to use the userRef for future uses
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();

cdfc8ee5be1b3e56c0ff4f2563bfef3b13eab937
bche536 <bche536@aucklanduni.ac.nz>
2021-02-22 02:06:43 +1300

    Finished lecture 115

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
+import store from "./redux/store";
+
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </React.StrictMode>
-    , document.getElementById("root")
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
